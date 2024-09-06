ef3005d076aea11833597e2eb5cf69a9b144b99b
NSujith089 <nsujith089@gmail.com>
2024-09-05 20:37:16 -0500

    One

---
+++ 01Day.py
@@ -0,0 +1,50 @@
+print("Hello World!!")
+# print("%s,Sujith",% (N))
+# print("My name is {  } and I am from { }".format("Sujith, Dallas")
+      
+a = "45.7"     
+print(a)
+print(type(a))
+print(float(a))
+
+b = 5
+c = 10.2
+d = b+float(a)
+print(d)
+
+x = "Harry"
+y = 10
+z = 12.4
+
+w1 = x+str(y)
+w2 = y+int(z)
+w3 = z+float(y)
+print(w1,w2,w3)
+
+# usecase for converting the data types
+
+prompt = "Hi!!"
+user_input = input(prompt)
+user_value = input(prompt)
+print("Hello "+ user_input,user_value)
+
+# Using input keyword, we can inputs from console or create interactive sessions with users.
+
+
+value1 = int(input("enter a number1: "))
+value2 = int(input("enter a number2: "))
+print(value1 + value2)
+
+# Using input keyword, we can inputs from console or create interactive sessions with users like adding integers.
+
+
+x = value1
+if int(x) >= 10:
+    print("Hi")
+else:
+    print("Bye")
+    
+#Done for today
+# Date - Sept5th,2024
+
+    

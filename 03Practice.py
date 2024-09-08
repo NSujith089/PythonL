1b48540331154eaf367221a75b162ebfceef3aca
NSujith089 <nsujith089@gmail.com>
2024-09-08 17:04:31 -0500

    Jumping statements

---
+++ 03Practice.py
@@ -0,0 +1,19 @@
+"""
+Jumping Statements:
+1.Pass --> Ignore
+2.Break --> Terminate
+3.Continue --> Skip
+"""
+
+#if True:
+#    pass  #Using we can avoid error and add the statements later.#
+
+#for i in "Pythonlife":
+#    if i == 'l':
+#        break
+#    print(i)
+
+for i in "Pythonlife":
+    if i == 'y':
+        continue
+    print(i)
